faf9ed29733e2571b041b2c66dd96c6359ae64f9
imholynx <imholynx@gmail.com>
2018-07-18 11:02:25 +0300

    --

--- app/src/main/java/com/imholynx/abbyytestapp/data/source/TasksRepository.kt
+++ app/src/main/java/com/imholynx/abbyytestapp/data/source/TasksRepository.kt
@@ -2,14 +2,15 @@ package com.imholynx.abbyytestapp.data.source
 
 import com.imholynx.abbyytestapp.data.Task
 import com.imholynx.abbyytestapp.data.TaskStatus
+import com.imholynx.abbyytestapp.data.source.local.TasksLocalDataSource
 
-class TasksRepository():TasksDataSource{
+class TasksRepository(val tasksLocalDataSource: TasksDataSource):TasksDataSource{
 
     companion object {
         private var INSTANCE: TasksRepository? = null
 
-        @JvmStatic fun getInstance():TasksRepository{
-            return INSTANCE ?: TasksRepository().apply { INSTANCE = this }
+        @JvmStatic fun getInstance(tasksLocalDataSource: TasksLocalDataSource):TasksRepository{
+            return INSTANCE ?: TasksRepository(tasksLocalDataSource).apply { INSTANCE = this }
         }
 
         @JvmStatic fun destroyInstance(){
@@ -18,7 +19,6 @@ class TasksRepository():TasksDataSource{
     }
 
     override fun getTasks(callback: TasksDataSource.LoadTasksCallBack) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getTask(id: String, callback: TasksDataSource.GetTaskCallback) {
